1f99595da64de6734935729740d9cab6c4c0f7d5
jesnadolski <nadolski.jessica@gmail.com>
2020-12-19 09:52:58 -0500

    12/19/2020

---
+++ index.js
@@ -0,0 +1,5 @@
+function submitForm(){
+    window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+    formbutton("create", {action: "https://formspree.io/f/mdopwarn"})
+    frm.reset();
+};
